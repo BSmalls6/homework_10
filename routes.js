3ec63889feca94b031e92e8d28d63d9e515d76aa
BSmalls6 <smallwoodbrennan@gmail.com>
2019-12-05 18:48:53 -0800

    almost there

--- routes.js
+++ routes.js
@@ -5,11 +5,34 @@ var path = require("path");
 
 module.exports = function(app) {
  
-  app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
+//   app.get("/notes", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./public/notes.html"));
+//   });
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+//   app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./public/index.html"));
+//   });
+
+//   app.get("/api/notes", function(req, res) {
+//     false.sendFile(path.join(__dirname, "./db/db.json"), "utf8")
+//   });
+// };
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("/api/notes", function (req, res) {
+  readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+      .then(function (data) {
+          return res.json(JSON.parse(data));
+      });
+});
+app.post("/api/notes", function (req, res) {
+  var newNote = req.body;
+  readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+});
+
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var fs = require("fs");
-
+var path = require("path")
 var app = express();
 
 var PORT = process.env.PORT || 3000;
@@ -8,25 +8,68 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "./homework_10")));
 
-app.get("public/assets/js/index.js", function(req, res) {
-    res.json(activeNote);
+// pathing
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./db/db.json"), "utf8")
+      .then(function (res) {
+          return res.json(JSON.parse(res));
+      });
+});
+
+
+app.post("/api/notes", function (req, res) {
+  var newNote = req.body;
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8")
+});
+
+// end pathing
+// actions
+app.get("/api/notes", function(req, res) {
+fs.readFile(path.join("./db/db", res , (err)=>{
+  if (err) throw (err);
+}))
   });
 
   
-  app.post("./journal.json", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    fs.writeFile("newNote", activeNote, (err)=>{
-        if (err) throw (err);
+  app.post("/api/notes", function(req, res) {
+let note = req.body;
+    let noteData = fs.readFile(path.join("./db/db", res , (err)=>{
+      if (err) throw (err);
+    }));
+    let newNote = JSON.parse(noteData);
+
+    newNote.push(note);
+  
+    fs.writeFile("newNote", newNote, (err)=>{
+        if (err) throw (console.log(err));
         console.log("new note success wrote");
     });
      
     
   });
-  
 
+  app.delete('/delete/:id', function (req, res) {
+
+    fs.readFile(__dirname + "/api/notes", 'utf8', function (err, data) {
+        data = JSON.parse(data);
+        data = data.filter(x=>x.id!=req.params.id);
+        console.log(data);
+        fs.writeFile('users.json', JSON.stringify(data), function (err) {
+            if(err){return console.log(err);}
+        });
+    });
+});
+  
+// end of actions
 
 require("./routes")(app);
 
